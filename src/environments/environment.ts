320c4dec02f26a1955b9655307a8901f32999960
Kinga Johanna Szabó <kinga.johanna.szabo@zooshgroup.com>
2023-06-19 20:38:11 +0200

    add stable map

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapbox: {
+    accessToken:
+      'pk.eyJ1Ijoia2luZ2Fqb2hhbm5hIiwiYSI6ImNsaWdsMWdnbDBxamMzZHQ0bWdvZ2RwOXMifQ.9VhSZLftgjHoEXrID76tkQ',
+  },
 };
 
 /*
