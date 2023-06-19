59a2228b174da14cbf4ae82593e36f9082e5c3d9
Kinga Johanna Szabó <kinga.johanna.szabo@zooshgroup.com>
2023-06-19 20:38:11 +0200

    add stable map

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-map',
@@ -9,6 +11,43 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   standalone: true,
   imports: [IonicModule, ExploreContainerComponent],
 })
-export class mapPage {
+export class mapPage implements AfterViewInit, OnInit {
+  map: mapboxgl.Map | undefined;
   constructor() {}
+
+  ngOnInit() {
+    mapboxgl.accessToken = environment.mapbox.accessToken;
+    this.map = new mapboxgl.Map({
+      container: 'mapbox',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-74.5, 40],
+      zoom: 9,
+    });
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      if (this.map) this.map.resize();
+    }, 0);
+  }
+
+  flyToLocation() {
+    if (this.map) {
+      this.map.flyTo({
+        center: [-73.985, 40.748817],
+        zoom: 12,
+      });
+    }
+  }
+
+  adjustCamera() {
+    if (this.map) {
+      this.map.easeTo({
+        pitch: 60,
+        bearing: 180,
+        duration: 1000,
+        easing: (t: number) => t,
+      });
+    }
+  }
 }
