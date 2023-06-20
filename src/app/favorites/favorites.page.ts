a855f0b85adb3f77b5344e858950010f6b5d27d1
Kinga Johanna Szabó <kinga.johanna.szabo@zooshgroup.com>
2023-06-20 14:20:11 +0200

    fix favs

--- src/app/favorites/favorites.page.ts
+++ src/app/favorites/favorites.page.ts
@@ -1,38 +1,32 @@
 import { Component } from '@angular/core';
 
-
 import { HttpClient } from '@angular/common/http';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import { CustomHeaderComponent } from '../custom-header/custom-header.component'
-
-
+import { CustomHeaderComponent } from '../custom-header/custom-header.component';
+import { BehaviorSubject } from 'rxjs';
+import { Stop } from '../../types/stop';
+import { busRoutes } from '../../data/lines';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: 'favorites.page.html',
   styleUrls: ['favorites.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent, CustomHeaderComponent],
+  imports: [
+    IonicModule,
+    ExploreContainerComponent,
+    CustomHeaderComponent,
+    CommonModule,
+  ],
 })
-
 export class favoritesPage {
-  stops: any[];
+  stops$ = new BehaviorSubject<Stop[]>([]);
 
-  constructor(private http: HttpClient) {
-    this.stops = [];
+  constructor() {
+    this.stops$ = new BehaviorSubject(busRoutes[0].stops);
   }
 
-  ionViewDidEnter() {
-    this.http.get<any[]>('/assets/lines/line1.json').subscribe(data => {
-      this.stops = data.slice(0, 2);
-
-      this.http.get<any[]>('/assets/lines/line1.json').subscribe(dataline2 => {
-        this.stops = this.stops.concat(dataline2.slice(0, 2)); 
-      });
-
-    });
-
-  }  
-
+  ionViewDidEnter() {}
 }
