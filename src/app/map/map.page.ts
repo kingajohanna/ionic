02765a797a1912180f6f9950dcee45969861c932
Kinga Johanna Szabó <kinga.johanna.szabo@zooshgroup.com>
2023-06-20 13:49:26 +0200

    add before bus plan

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  CUSTOM_ELEMENTS_SCHEMA,
+  Component,
+  OnInit,
+  ViewContainerRef,
+} from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import mapboxgl from 'mapbox-gl';
@@ -6,6 +12,13 @@ import { environment } from 'src/environments/environment';
 import { Point } from '../../types/point';
 import { BehaviorSubject } from 'rxjs';
 import { busRoutes } from '../../data/lines';
+
+import { Stop } from '../../types/stop';
+import { Route } from '../../types/route';
+import { getRouteDirection } from '../../calculations/getRouteDirection';
+
+const latitudeThreshold = 0.0003;
+const longitudeThreshold = 0.0003;
 
 const camplusPos: Point = { longitude: 13.395538, latitude: 42.34656 };
 const forteSpagnoloPos: Point = { longitude: 13.404768, latitude: 42.355627 };
@@ -15,67 +28,170 @@ const forteSpagnoloPos: Point = { longitude: 13.404768, latitude: 42.355627 };
   styleUrls: ['map.page.scss'],
   standalone: true,
   imports: [IonicModule, ExploreContainerComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class mapPage implements AfterViewInit, OnInit {
-  map: mapboxgl.Map | undefined;
+  map: mapboxgl.Map;
   location$: BehaviorSubject<Point>;
+  destination$: BehaviorSubject<Point>;
+  destinationMarker: mapboxgl.Marker;
+  locationMarker: mapboxgl.Marker;
+  route$: BehaviorSubject<Route>;
 
-  constructor() {
-    this.location$ = new BehaviorSubject(forteSpagnoloPos);
-  }
+  constructor() {}
 
   ngOnInit() {
+    this.location$ = new BehaviorSubject(forteSpagnoloPos);
+    this.destination$ = new BehaviorSubject(forteSpagnoloPos);
+    this.route$ = new BehaviorSubject({
+      beforeBus: [],
+      onBus: [],
+      afterBus: [],
+    } as Route);
+
     mapboxgl.accessToken = environment.mapbox.accessToken;
     this.map = new mapboxgl.Map({
       container: 'mapbox',
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [this.location$.value.longitude, this.location$.value.latitude],
-      zoom: 14,
+      zoom: 15,
+    });
+
+    this.destination$.subscribe((destination) => {
+      if (this.destinationMarker) {
+        this.destinationMarker.setLngLat([
+          destination.longitude,
+          destination.latitude,
+        ]);
+      } else {
+        this.destinationMarker = new mapboxgl.Marker({ color: 'red' })
+          .setLngLat([destination.longitude, destination.latitude])
+          .addTo(this.map);
+      }
+    });
+
+    this.location$.subscribe((location) => {
+      if (this.locationMarker) {
+        this.locationMarker.setLngLat([location.longitude, location.latitude]);
+      } else {
+        this.locationMarker = new mapboxgl.Marker()
+          .setLngLat([location.longitude, location.latitude])
+          .addTo(this.map);
+      }
+    });
+
+    this.route$.subscribe((route) => {
+      if (this.map.getLayer('beforebusLayer')) {
+        this.map.removeLayer('beforebusLayer');
+        this.map.removeSource('beforebus');
+      }
+
+      this.map.addSource('beforebus', {
+        type: 'geojson',
+        data: {
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'LineString',
+            coordinates: route.beforeBus,
+          },
+        },
+      });
+
+      this.map.addLayer({
+        id: 'beforebusLayer',
+        type: 'line',
+        source: 'beforebus',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round',
+        },
+        paint: {
+          'line-color': '#888',
+          'line-width': 8,
+        },
+      });
+    });
+
+    const self = this;
+
+    this.map.on('click', async function (e) {
+      if (
+        Math.abs(self.destination$.value.latitude - e.lngLat.lat) >
+          latitudeThreshold ||
+        Math.abs(self.destination$.value.longitude - e.lngLat.lng) >
+          longitudeThreshold
+      ) {
+        self.destination$.next({
+          longitude: e.lngLat.lng,
+          latitude: e.lngLat.lat,
+        });
+      } else {
+        const route = await getRouteDirection(
+          self.location$.value,
+          self.destination$.value
+        );
+        console.log('plan', route);
+
+        self.route$.next(route);
+      }
     });
   }
 
   ngAfterViewInit() {
     setTimeout(() => {
-      if (this.map) this.map.resize();
+      this.map.resize();
     }, 0);
 
     this.generateMarkers();
   }
 
   flyToLocation() {
-    if (this.map) {
-      this.map.flyTo({
-        center: [this.location$.value.longitude, this.location$.value.latitude],
-        zoom: 14,
-      });
-    }
+    this.map.flyTo({
+      center: [this.location$.value.longitude, this.location$.value.latitude],
+      zoom: 15,
+    });
   }
 
   adjustCamera() {
-    if (this.map) {
-      this.map.easeTo({
-        pitch: 60,
-        bearing: 180,
-        duration: 1000,
-        easing: (t: number) => t,
-      });
-    }
+    this.map.easeTo({
+      pitch: 60,
+      bearing: 180,
+      duration: 1000,
+      easing: (t: number) => t,
+    });
   }
 
   generateMarkers(): void {
     busRoutes.map((route) =>
       route.stops.map((stop) => {
-        var el = document.createElement('div');
-        el.className = 'marker';
-        el.style.backgroundImage = 'url(../../assets/busStation.png)';
-        el.style.width = '20px';
-        el.style.height = '20px';
-        el.style.backgroundSize = 'cover';
-
-        new mapboxgl.Marker(el)
-          .setLngLat([stop.point.longitude, stop.point.latitude])
-          .addTo(this.map!);
+        this.addMarker(
+          [stop.point.longitude, stop.point.latitude],
+          'url(../../assets/busStation.png)'
+        );
       })
     );
+  }
+
+  /** 
+          let markers: any[] = [];
+    busRoutes.map((route) => {
+      const stops = route.stops.map((stop) => ({
+        stop,
+      }));
+      markers = [...markers, ...stops];
+    });
+    this.busMarkers = markers;
+        ); */
+
+  addMarker(point: mapboxgl.LngLatLike, icon: string): void {
+    var el = document.createElement('div');
+    el.className = 'marker';
+    el.style.backgroundImage = icon;
+    el.style.width = '20px';
+    el.style.height = '20px';
+    el.style.backgroundSize = 'cover';
+
+    new mapboxgl.Marker(el).setLngLat(point).addTo(this.map!);
   }
 }
